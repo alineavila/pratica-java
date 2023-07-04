e217c3e29151e50a400726db08c573f058dbb58d
Aline Avila <aline.avila01@gmail.com>
2023-07-04 00:56:10 -0300

    classe abstrata - implementação padrão

--- src/oo/composicao/heranca/desafio/Luxo.java
+++ src/oo/composicao/heranca/desafio/Luxo.java
@@ -2,5 +2,17 @@ package oo.composicao.heranca.desafio;
 
 public interface Luxo {
     void ligarAr();
-    void desligarAr();
+
+    default void desligarAr() {
+        return;
+    }
+
+    /*
+    * É possível dar uma implementação padrão para um método em uma interface
+    */
+
+    default int velocidadeDoAr() {
+        return 1;
+    }
+
 }
